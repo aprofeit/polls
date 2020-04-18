cd694a114495c79ad420d4c77600e07aeb51dad8
Alexander Profeit <alex@profeit.com>
2020-04-18 11:13:13 -0400

    allow answering and add results page

--- pages/polls/[token].js
+++ pages/polls/[token].js
@@ -1,21 +1,50 @@
 import Layout from '../../components/layout'
 import axios from 'axios'
+import Form from 'react-bootstrap/form'
+import Button from 'react-bootstrap/button'
+import Router from 'next/router'
 
 class Poll extends React.Component {
+  state = {
+    answer: null
+  }
+
+  handleSelect = (e) => {
+    this.setState({
+      answer: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const host = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://polls-api.profeit.com"
+    axios.post(
+      `${host}/polls/${this.props.poll.token}/poll_responses`,
+      { poll_response: { answer: this.state.answer } },
+      { headers: { 'Content-Type': 'application/json' } }
+    ).then(response => {
+      Router.push(`/polls/${this.props.poll.token}/results`)
+    })
+  }
+
   render() {
     return (
       <Layout>
-
         <h2 className="mt-4 mb-4">{this.props.poll.question}</h2>
 
-        <div className="mb-3">
-          <code>
-            {this.props.poll.answer_list.map(function(answer) {
-              return (<li>{answer}</li>)
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            {this.props.poll.answer_list.map(answer => {
+              return (
+                <Form.Check custom type="radio" id={`option-${answer}`} label={answer} name="answer" value={answer} onClick={this.handleSelect} />
+              )
             })}
-          </code>
-        </div>
-        <p className="font-weight-lighter">This is where you'd normally choose an answer. But this isn't done.</p>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Respond
+          </Button>
+        </Form>
       </Layout>
     )
   }
---
+++ pages/polls/[token]/results.js
@@ -0,0 +1,11 @@
+import Layout from '../../../components/layout'
+
+export default class Results extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <h1 className="mt-4 mb-4">Thanks</h1>
+      </Layout>
+    )
+  }
+}
