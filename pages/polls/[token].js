fa975eb4c6b1fd1ed131cd2b7dff9075d3c5e5e4
Alexander Profeit <aprofeit@hyperion2.local>
2020-05-22 19:09:14 -0400

    fix endpoints

--- pages/polls/[token].js
+++ pages/polls/[token].js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { FaChevronLeft } from 'react-icons/fa'
 
 class Poll extends React.Component {
-  apiHost = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://polls-api.profeit.com"
+  apiHost = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://polls.profeit.com"
 
   state = {
     answer: null
@@ -21,7 +21,7 @@ class Poll extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const host = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://polls-api.profeit.com"
+    const host = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://polls-api.profeit.com"
     axios.post(
       `${host}/polls/${this.props.poll.token}/poll_responses`,
       { poll_response: { answer: this.state.answer } },
